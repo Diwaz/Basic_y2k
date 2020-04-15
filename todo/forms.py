0ad9a96435a482a8851b28d8710e3ad2a2949dbd
diwaz <diwaswatson@gmail.com>
2020-04-15 14:53:11 +0545

    Updated UI
    loaded bootstrap,
    crispyforms

--- todo/forms.py
+++ todo/forms.py
@@ -1,9 +1,14 @@
 from django import forms
-from . import models
+from .models import Todo
 
-class Todo(forms.ModelForm):
+class Todoforms(forms.ModelForm):
     
     class Meta:
         model = Todo
-        fields = ("__all__",)
+        fields = ("__all__")
+        labels  = {
+            'title': 'Title',
+            'description': 'Description'
+
+        }
 
--- todo/views.py
+++ todo/views.py
@@ -1,6 +1,14 @@
-from django.shortcuts import render,HttpResponse
+from django.shortcuts import render,redirect
+from .forms import Todoforms
+from .models import Todo
 
 
 
 def index(request):
     return render(request,"base.html")
+
+
+def form(request):
+    form = Todoforms()
+    return render(request,"todo_form.html",{'form':form})
+
