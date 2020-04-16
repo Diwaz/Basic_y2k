891de3d79788260f9b0ac798c23918663fae3dff
diwaz <diwaswatson@gmail.com>
2020-04-16 23:20:56 +0545

    Register Done
    All Set For register

--- accounts/views.py
+++ accounts/views.py
@@ -2,9 +2,7 @@ from django.shortcuts import render,redirect
 from django.contrib.auth.models import User,auth
 
 def register(request):
-    if request.method == 'GET':
-        return render(request,'register.html')
-    else:
+    if request.method == 'POST':
         first_name = request.POST['first_name']
         last_name = request.POST['last_name']
         username = request.POST['username']
@@ -12,10 +10,14 @@ def register(request):
         pass1 = request.POST['password1']
         pass2 = request.POST['password2']
 
-        user=User.objects.create_user(username=username,first_name=first_name,last_name=last_name,password=pass1,email=email)
+        user=User.objects.create_user(username=username, first_name=first_name, last_name=last_name, password=pass1, email=email)
         user.save()
         print('User Created')
-        return redirect('home/')
+        return render(request,'todo_list.html')
+       
+    else:
+         return render(request,'register.html')
+       
 
 def home(request):
     return render(request,'homepage.html')
