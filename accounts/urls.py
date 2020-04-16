6360de66d5b5e46bff463a01446c8206cc337222
diwaz <diwaswatson@gmail.com>
2020-04-16 22:37:01 +0545

    deleted home
    homepage is deleted for now

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,8 +2,8 @@ from django.urls import path,include
 from . import views
 
 urlpatterns = [
-    path('', views.home,name='homepage'),
-    path('register/',views.register,name='register'),
+    path('', views.register,name='register'),
+    
     
   
 ]
